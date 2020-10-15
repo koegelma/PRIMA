551165c772bbc333d72794a0c9c7e66abcf3dfd5
Marius Kögel <56830035+koegelma@users.noreply.github.com>
2020-10-15 14:48:56 +0200

    main.ts erweitert

--- L01_FirstFudge/Main.js
+++ L01_FirstFudge/Main.js
@@ -4,5 +4,24 @@ var L01_FirstFudge;
     console.log("Hallo");
     var fc = FudgeCore;
     console.log(fc);
+    window.addEventListener("load", hndLoad);
+    function hndLoad(_event) {
+        const canvas = document.querySelector("canvas");
+        fc.Debug.log(canvas);
+        let node = new fc.Node("Quad");
+        let mesh = new fc.MeshQuad();
+        let cmpMesh = new fc.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+        let mtrSolidWhite = new fc.Material("SolidWhite", fc.ShaderUniColor, new fc.CoatColored(fc.Color.CSS("WHITE")));
+        let cmpMaterial = new fc.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
+        let cmpCamera = new fc.ComponentCamera();
+        cmpCamera.pivot.translateZ(2);
+        cmpCamera.pivot.rotateY(180);
+        L01_FirstFudge.viewport = new fc.Viewport();
+        L01_FirstFudge.viewport.initialize("Viewport", node, cmpCamera, canvas);
+        fc.Debug.log(L01_FirstFudge.viewport);
+        L01_FirstFudge.viewport.draw();
+    }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=Main.js.map
--- L01_FirstFudge/Main.ts
+++ L01_FirstFudge/Main.ts
@@ -3,5 +3,31 @@ namespace L01_FirstFudge {
     import fc = FudgeCore;
 
     console.log(fc);
+    window.addEventListener("load", hndLoad);
+    export let viewport: fc.Viewport;
 
+    function hndLoad(_event: Event): void {
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        fc.Debug.log(canvas);
+
+        let node: fc.Node = new fc.Node("Quad");
+
+        let mesh: fc.MeshQuad = new fc.MeshQuad();
+        let cmpMesh: fc.ComponentMesh = new fc.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+        
+        let mtrSolidWhite: fc.Material = new fc.Material("SolidWhite", fc.ShaderUniColor, new fc.CoatColored(fc.Color.CSS("WHITE")));
+        let cmpMaterial: fc.ComponentMaterial = new fc.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
+
+        let cmpCamera: fc.ComponentCamera = new fc.ComponentCamera();
+        cmpCamera.pivot.translateZ(2);
+        cmpCamera.pivot.rotateY(180);
+
+        viewport = new fc.Viewport();
+        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        fc.Debug.log(viewport);
+
+        viewport.draw();
+    }
 }

0079bbcd77e6061f9bfb8f620a94a890ff0270de
koegelma <marius.koegel@hs-furtwangen.de>
2020-10-26 14:07:57 +0100

    Inserted rows & columns in obstacles

--- L03_Breakout/Main.js
+++ L03_Breakout/Main.js
@@ -4,18 +4,18 @@ var L03_Breakout;
     var fc = FudgeCore;
     window.addEventListener("load", hndLoad);
     let ball;
-    let velocity = new fc.Vector3(fc.Random.default.getRange(-1, 1), fc.Random.default.getRange(-1, 0), 0);
+    let velocity = new fc.Vector3(fc.Random.default.getRange(-0.5, 0.5), fc.Random.default.getRange(-0.5, 0), 0);
     //let velocity: fc.Vector3 = new fc.Vector3(10, -10, 0);
     let speed = 18;
     velocity.normalize(speed);
     let paddle;
     let keysPressed = {};
-    let obstacle = new Array(22);
+    let obstacle = new Array(100);
     let root;
     function hndLoad(_event) {
         const canvas = document.querySelector("canvas");
         root = new fc.Node("Root");
-        // Ball
+        // Setup Ball
         ball = new fc.Node("Ball");
         ball.addComponent(new fc.ComponentTransform());
         let meshQuad = new fc.MeshQuad();
@@ -26,45 +26,58 @@ var L03_Breakout;
         ball.addComponent(cMaterial);
         root.addChild(ball);
         ball.mtxLocal.scale(new fc.Vector3(0.5, 0.5, 0.5));
-        // Collision Walls
+        // Setup Collision Walls
         root.appendChild(createNode("WallLeft", meshQuad, mtrSolidWhite, new fc.Vector2(-21, 0), new fc.Vector2(1, 27)));
         root.appendChild(createNode("WallRight", meshQuad, mtrSolidWhite, new fc.Vector2(21, 0), new fc.Vector2(1, 27)));
         root.appendChild(createNode("WallTop", meshQuad, mtrSolidWhite, new fc.Vector2(0, 14), new fc.Vector2(42, 1)));
         root.appendChild(createNode("WallBottom", meshQuad, mtrSolidWhite, new fc.Vector2(0, -14), new fc.Vector2(42, 1)));
-        // Paddle
+        // Setup Paddle
         paddle = createNode("Paddle", meshQuad, mtrSolidWhite, new fc.Vector2(0, -12), new fc.Vector2(5, 0.5));
         root.appendChild(paddle);
-        // Obstacle
+        // Setup Obstacles
         let mtrRed = new fc.Material("Red", fc.ShaderUniColor, new fc.CoatColored(fc.Color.CSS("RED")));
-        let xPos = 1;
-        for (let i = 0; i < 10; i++) {
-            obstacle[i] = createNode("Obstacle" + i, meshQuad, mtrRed, new fc.Vector2(-19 + xPos, 12), new fc.Vector2(3, 0.5));
+        let mtrDarkOrange = new fc.Material("DarkOrange", fc.ShaderUniColor, new fc.CoatColored(fc.Color.CSS("DARKORANGE")));
+        /* for (let i: number = 1; i < 38; i += 4) {
+            obstacle[i] = createNode("Obstacle" + i, meshQuad, mtrRed, new fc.Vector2(-19 + i, 12), new fc.Vector2(3, 0.5));
             root.appendChild(obstacle[i]);
-            xPos += 4;
+        } */
+        for (let row = 1; row < 6; row++) {
+            for (let col = 1; col < 38; col += 4) {
+                if (row > 2) {
+                    obstacle[row + col] = createNode("Obstacle" + row + col, meshQuad, mtrDarkOrange, new fc.Vector2(-19 + col, 14 - row), new fc.Vector2(3, 0.5));
+                }
+                else {
+                    obstacle[row + col] = createNode("Obstacle" + row + col, meshQuad, mtrRed, new fc.Vector2(-19 + col, 14 - row), new fc.Vector2(3, 0.5));
+                }
+                root.appendChild(obstacle[row + col]);
+            }
         }
-        // Camera
+        // Setup Camera
         let cmpCamera = new fc.ComponentCamera();
         cmpCamera.pivot.translateZ(40);
         cmpCamera.pivot.rotateY(180);
-        // Input
+        // Setup Input
         document.addEventListener("keydown", hndKeydown);
         document.addEventListener("keyup", hndKeyup);
-        // Viewport
+        // Setup Viewport
         L03_Breakout.viewport = new fc.Viewport();
         L03_Breakout.viewport.initialize("Viewport", root, cmpCamera, canvas);
         fc.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, hndLoop);
         fc.Loop.start(fc.LOOP_MODE.TIME_GAME, 60);
     }
     function hndLoop(_event) {
+        // Input
         if (keysPressed[fc.KEYBOARD_CODE.ARROW_LEFT])
-            paddle.cmpTransform.local.translate(new fc.Vector3(-0.5));
+            paddle.cmpTransform.local.translate(new fc.Vector3(-0.8));
         if (keysPressed[fc.KEYBOARD_CODE.ARROW_RIGHT])
-            paddle.cmpTransform.local.translate(fc.Vector3.X(0.5));
+            paddle.cmpTransform.local.translate(fc.Vector3.X(0.8));
         /*         console.log("X: " + velocity.x);
                 console.log("Y: " + velocity.y); */
+        // Ball Movement
         let frameTime = fc.Time.game.getElapsedSincePreviousCall() / 500;
         let move = fc.Vector3.SCALE(velocity, frameTime);
         ball.mtxLocal.translate(move);
+        // Detect Collision
         let hit = false;
         for (let node of root.getChildren()) {
             if (node.name == "Ball")
@@ -85,24 +98,19 @@ var L03_Breakout;
     }
     function processHit(_node) {
         fc.Debug.log("Reflect at ", _node.name);
-        //let hitsObstacle: number = 0;
+        // Obstacle Hit
         for (var i in obstacle) {
-            switch (_node.name) {
-                case "Obstacle" + i:
+            switch (_node) {
+                case obstacle[i]:
                     obstacle[i].cmpTransform.local.translate(new fc.Vector3(-50, 0, 0));
-                    velocity.y *= -1;
-                    /* hitsObstacle++;
-                    if (hitsObstacle >= 2) {
-                        velocity.y *= 10;
-                        velocity.x *= 10;
-                    }
-                    if (hitsObstacle >= 4) {
-                        velocity.y *= 10;
-                        velocity.x *= 10;
-                    } */
+                    velocity.y *= -1.02;
+                    let x = 1;
+                    x -= 0.01;
+                    paddle.mtxLocal.scaleX(x);
                     break;
             }
         }
+        // Collision
         switch (_node.name) {
             case "WallTop":
                 velocity.y *= -1;
@@ -121,9 +129,9 @@ var L03_Breakout;
             case "Paddle":
                 velocity.y *= -1;
                 break;
-            default:
+            /* default:
                 fc.Debug.warn("Oh, no, I hit something unknown!!", _node.name);
-                break;
+                break; */
         }
     }
     function createNode(_name, _mesh, _material, _translation, _scaling) {
--- L03_Breakout/Main.ts
+++ L03_Breakout/Main.ts
@@ -8,13 +8,13 @@ namespace L03_Breakout {
 
     window.addEventListener("load", hndLoad);
     let ball: fc.Node;
-    let velocity: fc.Vector3 = new fc.Vector3(fc.Random.default.getRange(-1, 1), fc.Random.default.getRange(-1, 0), 0);
+    let velocity: fc.Vector3 = new fc.Vector3(fc.Random.default.getRange(-0.5, 0.5), fc.Random.default.getRange(-0.5, 0), 0);
     //let velocity: fc.Vector3 = new fc.Vector3(10, -10, 0);
     let speed: number = 18;
     velocity.normalize(speed);
     let paddle: fc.Node;
     let keysPressed: KeyPressed = {};
-    let obstacle: fc.Node[] = new Array(22);
+    let obstacle: fc.Node[] = new Array(100);
 
     export let viewport: fc.Viewport;
     let root: fc.Node;
@@ -24,7 +24,7 @@ namespace L03_Breakout {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
         root = new fc.Node("Root");
 
-        // Ball
+        // Setup Ball
 
         ball = new fc.Node("Ball");
         ball.addComponent(new fc.ComponentTransform());
@@ -38,41 +38,54 @@ namespace L03_Breakout {
 
         ball.mtxLocal.scale(new fc.Vector3(0.5, 0.5, 0.5));
 
-        // Collision Walls
+        // Setup Collision Walls
 
         root.appendChild(createNode("WallLeft", meshQuad, mtrSolidWhite, new fc.Vector2(-21, 0), new fc.Vector2(1, 27)));
         root.appendChild(createNode("WallRight", meshQuad, mtrSolidWhite, new fc.Vector2(21, 0), new fc.Vector2(1, 27)));
         root.appendChild(createNode("WallTop", meshQuad, mtrSolidWhite, new fc.Vector2(0, 14), new fc.Vector2(42, 1)));
         root.appendChild(createNode("WallBottom", meshQuad, mtrSolidWhite, new fc.Vector2(0, -14), new fc.Vector2(42, 1)));
 
-        // Paddle
+        // Setup Paddle
 
-        
         paddle = createNode("Paddle", meshQuad, mtrSolidWhite, new fc.Vector2(0, -12), new fc.Vector2(5, 0.5));
         root.appendChild(paddle);
 
-        // Obstacle
+        // Setup Obstacles
 
         let mtrRed: fc.Material = new fc.Material("Red", fc.ShaderUniColor, new fc.CoatColored(fc.Color.CSS("RED")));
-        let xPos: number = 1;
-        for (let i: number = 0; i < 10; i++) {
-            obstacle[i] = createNode("Obstacle" + i, meshQuad, mtrRed, new fc.Vector2(-19 + xPos, 12), new fc.Vector2(3, 0.5));
+        let mtrDarkOrange: fc.Material = new fc.Material("DarkOrange", fc.ShaderUniColor, new fc.CoatColored(fc.Color.CSS("DARKORANGE")));
+
+        /* for (let i: number = 1; i < 38; i += 4) {
+            obstacle[i] = createNode("Obstacle" + i, meshQuad, mtrRed, new fc.Vector2(-19 + i, 12), new fc.Vector2(3, 0.5));
             root.appendChild(obstacle[i]);
-            xPos += 4;
+        } */
+
+        for (let row: number = 1; row < 6; row++) {
+            for (let col: number = 1; col < 38; col += 4) {
+
+                if (row > 2) {
+                    obstacle[row + col] = createNode("Obstacle" + row + col, meshQuad, mtrDarkOrange, new fc.Vector2(-19 + col, 14 - row), new fc.Vector2(3, 0.5));
+                } else {
+                    obstacle[row + col] = createNode("Obstacle" + row + col, meshQuad, mtrRed, new fc.Vector2(-19 + col, 14 - row), new fc.Vector2(3, 0.5));
+                }
+
+
+                root.appendChild(obstacle[row + col]);
+            }
         }
 
-        // Camera
+        // Setup Camera
 
         let cmpCamera: fc.ComponentCamera = new fc.ComponentCamera();
         cmpCamera.pivot.translateZ(40);
         cmpCamera.pivot.rotateY(180);
 
-        // Input
+        // Setup Input
 
         document.addEventListener("keydown", hndKeydown);
         document.addEventListener("keyup", hndKeyup);
 
-        // Viewport
+        // Setup Viewport
 
         viewport = new fc.Viewport();
         viewport.initialize("Viewport", root, cmpCamera, canvas);
@@ -87,9 +100,9 @@ namespace L03_Breakout {
         // Input
 
         if (keysPressed[fc.KEYBOARD_CODE.ARROW_LEFT])
-            paddle.cmpTransform.local.translate(new fc.Vector3(-0.5));
+            paddle.cmpTransform.local.translate(new fc.Vector3(-0.8));
         if (keysPressed[fc.KEYBOARD_CODE.ARROW_RIGHT])
-            paddle.cmpTransform.local.translate(fc.Vector3.X(0.5));
+            paddle.cmpTransform.local.translate(fc.Vector3.X(0.8));
 
         /*         console.log("X: " + velocity.x);
                 console.log("Y: " + velocity.y); */
@@ -113,7 +126,6 @@ namespace L03_Breakout {
                 break;
             }
         }
-
         viewport.draw();
     }
 
@@ -127,25 +139,21 @@ namespace L03_Breakout {
     function processHit(_node: fc.Node): void {
         fc.Debug.log("Reflect at ", _node.name);
 
-        //let hitsObstacle: number = 0;
+        // Obstacle Hit
 
         for (var i in obstacle) {
-            switch (_node.name) {
-                case "Obstacle" + i:
+            switch (_node) {
+                case obstacle[i]:
                     obstacle[i].cmpTransform.local.translate(new fc.Vector3(-50, 0, 0));
-                    velocity.y *= -1;
-                    /* hitsObstacle++;
-                    if (hitsObstacle >= 2) {
-                        velocity.y *= 10;
-                        velocity.x *= 10;
-                    }
-                    if (hitsObstacle >= 4) {
-                        velocity.y *= 10;
-                        velocity.x *= 10;
-                    } */
+                    velocity.y *= -1.02;
+                    let x: number = 1;
+                    x -= 0.01;
+                    paddle.mtxLocal.scaleX(x);
                     break;
             }
         }
+
+        // Collision
 
         switch (_node.name) {
             case "WallTop":
@@ -165,9 +173,9 @@ namespace L03_Breakout {
             case "Paddle":
                 velocity.y *= -1;
                 break;
-            default:
+            /* default:
                 fc.Debug.warn("Oh, no, I hit something unknown!!", _node.name);
-                break;
+                break; */
         }
     }
 
